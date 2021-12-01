bb7c1cb33d61566ad16cd8422fb3e9f76ee106bb
FabioCividini <fabiocividini22@gmail.com>
2021-12-01 16:19:47 +0100

    esercizio finito

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,11 +1,44 @@
 package org.generation.italy.christmas;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		ArrayList<String> listaDesideri = new ArrayList<String>();
 		
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int scelta;
+		boolean valido = false;
+		
+		do {
+			
+			System.out.println("Aggiungi elemento alla lista dei desideri: ");
+			scanner.next();
+			listaDesideri.add(scanner.nextLine());
+				
+			System.out.println("La tua lista contiene " + listaDesideri.size() + " desideri");
+			
+			
+			System.out.println("Continuare? (1 per SI o 2 per NO): ");
+			scelta = scanner.nextInt();
+			
+			
+			if(scelta == 2) {
+				valido = true;
+			}
+			
+			
+		} while(valido == false);
+		
+		
+		System.out.println("Ecco la tua lista:");
+		System.out.println(listaDesideri);
+	
+		
+		scanner.close();
 	}
 
 }
